c9b7541a134c0b73822a5b1aad50e30909646014
dmontesanto <d.montesanto@gmail.com>
2018-06-07 09:21:47 +0200

    + Added property service

---
+++ core/src/main/java/com/tmajoir/lib/core/model/node/NodeDetail.java
@@ -0,0 +1,23 @@
+package com.tmajoir.lib.core.model.node;
+
+public class NodeDetail {
+
+    private String nodeUuid;
+    private String address;
+
+    public String getNodeUuid() {
+        return nodeUuid;
+    }
+
+    public void setNodeUuid(String nodeUuid) {
+        this.nodeUuid = nodeUuid;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
